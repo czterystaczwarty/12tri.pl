5011356bb6fe99d7c2f4901b1bc17ed22bb03ab3
Tomasz Szopiński <czterystaczwarty@gmail.com>
2012-10-16 12:36:53 +0200

    responsive navigation (uncomplited)

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,16 @@
 $(function(){
+	var $pull = $('#pull'),
+		$menu = $('nav ul');
 
 	$('.posts').masonry({
 		itemSelector: '.post',
 		columnWidth: 240
+	});
+
+	$pull.on('click', function (e)
+	{
+		e.preventDefault();
+		$menu.slideToggle();
 	});
 
 });
