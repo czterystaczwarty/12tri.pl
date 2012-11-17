8e07c6a56588a6b38287b80c480f1a35eaf72573
Tomasz Szopiński <czterystaczwarty@gmail.com>
2012-11-17 22:48:30 +0100

    Fix in compass project config

--- wordpress/wp-content/themes/bp-12tri/config.rb
+++ wordpress/wp-content/themes/bp-12tri/config.rb
@@ -2,7 +2,7 @@ line_comments = false
 output_style = :expanded
 
 relative_assets = true
-http_path = "/wp-content/themes/bp-12tri/"
+http_path = "wp-content/themes/bp-12tri/"
 css_dir = "styles"
 sass_dir = "styles/source"
 images_dir = "images"
